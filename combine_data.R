9d7568c64251bf8e4d3459c699666d95e6c181f4
bryan.yang <bryan.yang@ringcentral.com>
2016-06-10 00:42:55 +0800

    refactor

--- combine_data.R
+++ combine_data.R
@@ -45,36 +45,5 @@ while (lower_x < max_mar){
 
 
 
-grid <- function(data, lower_x, lower_y, accuracy_threshold)
-{
-  upper_x <- lower_x + margin
-  upper_y <- lower_y + margin
-  data_sub <- data[(data$x >= lower_x & data$x < upper_x-0.0005
-                    & data$y >= lower_y & data$y < upper_y-0.0005
-                    & data$accuracy > accuracy_threshold),]
-  return(data_sub)
-}
-
-
-submission <- function (train, test){
-  
-  feature <- c("x", "y")
-  y <- "place_id"
-  train <- train[, feature, with=FALSE]
-  test_x <- test[, feature, with=FALSE]
-  test_id <- test[, "row_id", with=FALSE]
-  
-  model_1 <- knn(train, test_x, df_train_sub[[y]]  , k=1, prob = TRUE)
-  model_2 <- knn(train, test_x, df_train_sub[[y]]  , k=5, prob = TRUE)
-  model_3 <- knn(train, test_x, df_train_sub[[y]]  , k=10, prob = TRUE)
-  
-  submission <- cbind(test_id, as.character(model_1), as.character(model_2), as.character(model_3))
-  submission <- data.table(submission)
-  cols <- colnames(submission)[2:length(submission)]
-  #submission$place_id <- paste(c(submission$V1, submission$V2, submission$V3), sep =" ") 
-  setDT(submission)[, place_id := Reduce(function(...) paste(..., sep = " "), .SD[, mget(cols)])]
-  #str(submission)
-  return(submission[,c("row_id", "place_id"), with=FALSE])
-}
 
 

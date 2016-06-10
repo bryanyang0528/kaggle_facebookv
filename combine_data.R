08ab7df288f0c9b2953f1dfbdc7f3103f97c9c7f
bryan.yang <bryan.yang@ringcentral.com>
2016-06-10 08:20:22 +0800

    revised submission data

--- combine_data.R
+++ combine_data.R
@@ -5,7 +5,7 @@ library(class)
 
 margin <- 0.5
 min_mar <- 0
-max_mar <- 10
+max_mar <- 9.99
 accuracy_threshold <- 27
 set.seed(123)
 file_train = "data/train.csv"
@@ -18,7 +18,9 @@ df_test <- fread(file_test, header = TRUE, nrows = nrows, integer64 = "character
                  colClasses=c("character", "numeric", "numeric", "integer", "NULL"))
 
 # initialization 
-lower_x <- min_mar
+
+#lower_x <- min_mar
+lower_x <- 6
 submission_data <- data.table(row_id=character(),place_id=character())
 
 
@@ -31,7 +33,7 @@ while (lower_x < max_mar){
     
     #main function here 
     df_train_sub <- grid(df_train, lower_x, lower_y, accuracy_threshold)
-    df_test_sub <- grid(df_test, lower_x, lower_y, accuracy_threshold)
+    df_test_sub <- grid(df_test, lower_x, lower_y)
     submission_sub <- submission(df_train_sub, df_test_sub)
     submission_data <- rbind(submission_data, submission_sub)    
   
@@ -40,10 +42,15 @@ while (lower_x < max_mar){
   lower_x <- lower_x + margin
 }
 
+submission_data <- submission_data[order(as.numeric(row_id))]
+
+write.csv(submission_data,file = 'data/submission_data.csv',quote = FALSE,row.names = FALSE)
 
 
+# find missing data
 
+temp <- merge(df_test, submission_data, by.x="row_id", by.y="row_id",all = TRUE )
+head(temp)
+temp[is.na(temp$place_id),][order(x,y)]
 
-
-
-
+write.csv(temp[,c("row_id","place_id"),with=FALSE][order(as.numeric(row_id))], file = 'data/submission_data_temp.csv',quote = FALSE,row.names = FALSE)
